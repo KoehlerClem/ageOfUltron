0e67513a06426af22b3f2a9488acb37dfc7562c1
tomjbvvb <tomtroschuetz@web.de>
2015-03-04 15:36:40 +0100

    zufallsfunktion

--- workspace_simulation/simulation/simulation/src/RobolabSimClient.c
+++ workspace_simulation/simulation/simulation/src/RobolabSimClient.c
@@ -173,6 +173,17 @@ int main(void) {
 	printf("Token: %d\n", Robot_Move(1, 0));
 	printf("Intersection: %d\n", Robot_GetIntersections());
 	//printf("Token: %d\n", Robot_Move(2, 0));
+
+
+
+	void checkEdges(short currentEdges[]){
+	if (currentEdges[0]==3)
+	moveWest();
+
+	}
+	void moveRandom(){
+
+	}
 	*/
 
 
