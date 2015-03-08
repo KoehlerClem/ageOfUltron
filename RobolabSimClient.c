c76dd2e54f6fa5f7e99536decf971d8fcc7e66fa
KoehlerClem <koehler.clem@gmail.com>
2015-03-08 05:14:57 +0100

    Suche Kürzester Weg
    Funktioniert super, wenn er alle Punkte im Labyrinth abgelaufen ist dann
    hängt der Code sich noch auf aber Wayne. Sieht nicht hübsch aus aber es
    ist hier 5:14 also ja :dancer:

--- RobolabSimClient.c
+++ RobolabSimClient.c
@@ -3,11 +3,7 @@
 
 int main(void) {
 
-
-
-
 	// ---------------- Steak Area -----------------
-
 
 	/* Hier wird der Struct für einen Point definiert. Der Point hat x,y Coordinaten
 	 * in coord gespeichert, einen short Wert für die Kanten und einen bool-Wert des
@@ -16,22 +12,21 @@ int main(void) {
 	typedef struct points{
 		short coord[2];
 		short arrayEdges[4];
+		short pathToPointExists;
+		short edgeToPointBefore;
 		short visited;						// 1 = true, 0 = false
 	} point;
-
-
 
 	point allPoints[77];					// Das Array in dem Alle Punkte gespeichert werden
 	short currentArrayPoint = 1;			// Der Punkt an dem der letzte Punkt in dem Array erstellt wurde
 	short currentCoord[2] = {0,0};
 	short currentPoint = 0;					// Die aktuelle Coordinate des aktuellen Punktes
-	short nextMove = 1;
-	short lastPointVisited;
-	short nachbarPunkte[4];
+	short neighbourPoint[4];
 
 	//Füllt alle Points mit werten auf
 	short pointsfiller;
 	for ( pointsfiller = 0; pointsfiller < 77; pointsfiller++ ){
+		allPoints[pointsfiller].pathToPointExists = 0;
 		allPoints[pointsfiller].coord[0] = 0;
 		allPoints[pointsfiller].coord[1] = 0;
 		allPoints[pointsfiller].visited = 0;
@@ -42,24 +37,24 @@ int main(void) {
 	}
 
 	void edgeToArray(short pointInArray, int edges){
-			switch(edges){
-			case 16: allPoints[pointInArray].arrayEdges[0] = 1; break;
-			case 32: allPoints[pointInArray].arrayEdges[2] = 1; break;
-			case 16*3: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[2] = 1; break;
-			case 16*4: allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*5: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*6: allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*7: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*8: allPoints[pointInArray].arrayEdges[1] = 1; break;
-			case 16*9: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; break;
-			case 16*10: allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; break;
-			case 16*11: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; break;
-			case 16*12: allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*13: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*14: allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			case 16*15: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
-			}
+		switch(edges){
+		case 16: allPoints[pointInArray].arrayEdges[0] = 1; break;
+		case 32: allPoints[pointInArray].arrayEdges[2] = 1; break;
+		case 16*3: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[2] = 1; break;
+		case 16*4: allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*5: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*6: allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*7: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*8: allPoints[pointInArray].arrayEdges[1] = 1; break;
+		case 16*9: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; break;
+		case 16*10: allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; break;
+		case 16*11: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; break;
+		case 16*12: allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*13: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*14: allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
+		case 16*15: allPoints[pointInArray].arrayEdges[0] = 1; allPoints[pointInArray].arrayEdges[1] = 1; allPoints[pointInArray].arrayEdges[2] = 1; allPoints[pointInArray].arrayEdges[3] = 1; break;
 		}
+	}
 
 
 	/* Vergleicht zuerst die X-Coordinate des jeweils betrachteten Punktes gegen die übergebene X-Coordinate
@@ -85,17 +80,11 @@ int main(void) {
 		}
 	}
 
-
 	/* Es wird zuerst überprüft ob an den gegebenen Koordinaten schon ein Punk existiert.
 	 * Fals dies nicht der Fall ist wird ein neuer Punkt an den Koordinaten erstellt und
 	 * der Wert des current Arrays, also des derzeit vereachteten Punkts zurückgegeben.
 	 */
-
-
-
-
-
-	short getArrayAddressFromCoord(short coordToCheck[]){
+	short getArrayAddressFromCoord(short coordToCheck[2]){
 		short i;
 		for ( i = 0; i < currentArrayPoint; i++ ){
 			if ( coordToCheck[0] == allPoints[i].coord[0]){
@@ -125,65 +114,31 @@ int main(void) {
 	/* Wenn ein Punkt besucht wird, werden die Kanten des aktuellen Punktes gespeichert,
 	 * er als Besucht markiert und die 4 Umliegenden Punkte erstellt, fals es sie noch nicht gibt!
 	*/
-
-	void setNeighbourPoints(short coords[2]){
+	void setNeighbourPoints(short forThisPoint){
+		short coords[2] = {allPoints[forThisPoint].coord[0], allPoints[forThisPoint].coord[1]};
 		short northPoint[2] = {coords[0], coords[1] + 1};
 		short eastPoint[2] = {coords[0] + 1, coords[1]};
 		short southPoint[2] = {coords[0], coords[1] - 1};
 		short westPoint[2] = {coords[0] - 1, coords[1]};
-		nachbarPunkte[0] = getArrayAddressFromCoord(northPoint);
-		nachbarPunkte[1] = getArrayAddressFromCoord(eastPoint);
-		nachbarPunkte[2] = getArrayAddressFromCoord(southPoint);
-		nachbarPunkte[3] = getArrayAddressFromCoord(westPoint);
+		neighbourPoint[0] = getArrayAddressFromCoord(northPoint);
+		neighbourPoint[1] = getArrayAddressFromCoord(eastPoint);
+		neighbourPoint[2] = getArrayAddressFromCoord(southPoint);
+		neighbourPoint[3] = getArrayAddressFromCoord(westPoint);
 	}
 
-	void setNextMove(){
-
-		setNeighbourPoints(currentCoord);
-
-		// Überprüfen auf Kante und nicht besucht
-
-		short i;
-		for( i = 0; i < 4; i++){
-			if (allPoints[currentPoint].arrayEdges[i] == 1){
-				if (allPoints[nachbarPunkte[i]].visited == 0){
-					nextMove = i;
-					return;
-				}
-			}
-		}
-
-		for( i = 0; i < 4; i++){
-			if (allPoints[currentPoint].arrayEdges[i] == 1){
-				if (lastPointVisited != nachbarPunkte[i]){
-				nextMove = i;
-				return;
-				}
-			}
-		}
-
-		for( i = 0; i < 4; i++){
-			if (allPoints[currentPoint].arrayEdges[i] == 1){
-				nextMove = i;
-				return;
-			}
-		}
-	}
 
 
 
 	void visitPoint(){
-		lastPointVisited = currentPoint;
 		//Holt die Adresse in dem Array zu den Aktuellen Coordinaten
 		currentPoint = getArrayAddressFromCoord(currentCoord);
 		if ( allPoints[currentPoint].visited == 0){
 			edgeToArray(currentPoint, Robot_GetIntersections());
 			allPoints[currentPoint].visited = 1;
+			updatePoints();
 		}
-		updatePoints();
-		setNextMove();
-	}
 
+	}
 
 	/* Setzt den Punkt auf 0,0, empfohlen für den Start der Simulation
 	 */
@@ -256,33 +211,108 @@ int main(void) {
 
 	// ---------------- Bear Heaven -----------------
 
-	void getNewPath(){
-		short checkThisCoord[2] = {currentCoord[0], currentCoord[1]};
-		setNeighbourPoints(checkThisCoord);
+	void useNewPath(short pointToVisit){
+		printf("Starte useNewPath! Zu (%i,%i) über die Kante %i \n\n" , allPoints[pointToVisit].coord[0], allPoints[pointToVisit].coord[1], allPoints[pointToVisit].edgeToPointBefore);
+		short coords[2];
+		switch(allPoints[pointToVisit].edgeToPointBefore){
+		case 0: coords[0] = allPoints[pointToVisit].coord[0]; coords[1] = allPoints[pointToVisit].coord[1] - 1; useNewPath( getArrayAddressFromCoord(coords)); moveNorth(); break;
+		case 1: coords[0] = allPoints[pointToVisit].coord[0] - 1; coords[1] = allPoints[pointToVisit].coord[1]; useNewPath( getArrayAddressFromCoord(coords)); moveEast(); break;
+		case 2: coords[0] = allPoints[pointToVisit].coord[0]; coords[1] = allPoints[pointToVisit].coord[1] + 1; useNewPath( getArrayAddressFromCoord(coords)); moveSouth(); break;
+		case 3: coords[0] = allPoints[pointToVisit].coord[0] + 1; coords[1] = allPoints[pointToVisit].coord[1]; useNewPath( getArrayAddressFromCoord(coords)); moveWest(); break;
+		case -1: break;
+		}
+	}
 
+	short getNextPoint(){
+		short z;
+
+		printf("\nStart getNextPoint vom Punkt (%i,%i) aus! \n", currentCoord[0], currentCoord[1]);
+		short i;		//Läuft in der for-schleife von 0-3 für alle Richtungen
+		short nextPointToVisit = 0;			//Der nächste Punkt der noch nicht besucht ist
+		short buffer1[20] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};		// Hier sind die Punkte gespeicher die geprüft werden
+		short buffer2[20] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};		// Hier sind die nächsten Punkte die untersucht werden sollen
+		short indexBuffer1 = 0;		// Laufvariable die den ersten Buffer durchläuft
+		short indexBuffer2;		// Laufvariable für den 2. Buffer
+		short checkThisPoint;		//Der Punkt dessen Nachbarn als nächstes überprüft werden sollen
+
+		for(i = 0; i < 77; i++){		// Setzt für alle Punkte zurück, dass es einen Pfad zu ihnen gibt
+			allPoints[i].pathToPointExists = 0;
+		}
+		for(i = 0; i < 77; i++){		// Setzt für alle Punkte zurück, dass es einen Pfad zu ihnen gibt
+			allPoints[i].edgeToPointBefore = -1;
+
+		}
+		allPoints[currentPoint].edgeToPointBefore = -1;
+		allPoints[currentPoint].pathToPointExists = 1;
+		setNeighbourPoints(currentPoint);		// Sucht die Nachbarpunkte des aktuellen Punktes
+		for( i = 0; i < 4; i++){		//Füllt Buffer1 für den ersten Durchlauf mit den Nachbarpunkten auf die eine Kante (i) zu dem Punkt haben
+			if(allPoints[currentPoint].arrayEdges[i] == 1){
+				buffer1[indexBuffer1] = neighbourPoint[i];
+				allPoints[neighbourPoint[i]].edgeToPointBefore = i;			// ist der Weg zu dem Punkt die Kante i von dem jetzigem Punkt aus
+				allPoints[neighbourPoint[i]].pathToPointExists = 1;
+				if(allPoints[buffer1[indexBuffer1]].visited == 0){
+					return nextPointToVisit = buffer1[indexBuffer1];
+				}
+				indexBuffer1++;
+			}
+		}
+
+		while(nextPointToVisit == 0){		// => Solange kein Punkt gefunden ist der noch nicht besucht worden ist
+			printf("Starte Breitensuche \n");
+			indexBuffer1 = 0;		// Setzt den Indexbuffer auf den ersten Punkt im Buffer1-Array
+			indexBuffer2 = 0;		// Der nächste neue zu überprüfende Punkt wird im Buffer2 an die Position 0 geschrieben
+			printf("Betrachten jetzt den Punkt (%i,%i) mit Wegkante %i\n", allPoints[buffer1[indexBuffer1]].coord[0],allPoints[buffer1[indexBuffer1]].coord[1], allPoints[indexBuffer1].edgeToPointBefore);
+
+			while( buffer1[indexBuffer1] != -1){		// Überprüft ob es noch einen Punkt im Buffer1-Array gibt
+				printf("Lese Array 1 aus \n");
+				checkThisPoint = buffer1[indexBuffer1];		// Ließt aus dem Array die Adresse des zu überprüfenden Punkted aus
+
+				if(allPoints[checkThisPoint].visited == 0){		// Checkt ob der momentan betrachtete Punkt schon besucht worden ist
+					return nextPointToVisit = checkThisPoint;		// Fals ja, setzt den nächst zu besuchenden Point gleich dem momentan betrachteten Punkt
+				}
+
+				printf("Läd den Buffer2 mit den Nachbarn mit Kanten von dem betrachtetem Punkt\n");
+				setNeighbourPoints(checkThisPoint);		// Füllt das Array neightbourPoint mit den Nachbarpunkten des momentan betrachteten Punktes auf
+				for(i = 0; i < 4; i++){
+					if(allPoints[checkThisPoint].arrayEdges[i] == 1 && allPoints[neighbourPoint[i]].pathToPointExists == 0){		// Wenn es von dem momentan betrachtetem Punkt eine Kante in Richtung i gibt
+						if(allPoints[neighbourPoint[i]].visited == 0){		// und es zu dem Punkt in Richtung i keinen Weg gibt
+							allPoints[neighbourPoint[i]].edgeToPointBefore = i;			// ist der Weg zu dem Punkt die Kante i von dem jetzigem Punkt aus
+							allPoints[neighbourPoint[i]].pathToPointExists = 1;
+							return nextPointToVisit = neighbourPoint[i];
+						}
+						allPoints[neighbourPoint[i]].edgeToPointBefore = i;			// ist der Weg zu dem Punkt die Kante i von dem jetzigem Punkt aus
+						printf("\n\n Zielpunkt:(%i,%i) Erster Rücksprung:%i",allPoints[neighbourPoint[i]].coord[0], allPoints[neighbourPoint[i]].coord[1], allPoints[neighbourPoint[i]].edgeToPointBefore );
+						allPoints[neighbourPoint[i]].pathToPointExists = 1;
+						buffer2[indexBuffer2] = neighbourPoint[i];		// Speichert den gerade betrachteten Nachbarpunkt an der Kante i in das Buff
+						indexBuffer2++;
+					}
+				}
+				indexBuffer1++;
+			}
+
+			for (indexBuffer2 = 0; indexBuffer2 < 20; indexBuffer2++){		// Schreibt den Buffer2 in Buffer1 und leert Buffer2 wenn Buffer1 abgearbeitet ist
+				buffer1[indexBuffer2] = buffer2[indexBuffer2];
+				buffer2[indexBuffer2] = -1;
+			}
+		}
 	}
 
 
 
 	//endlosschelife
+
 	setNull();printStatusPoints();
+
 	short ever = 0;
-
 	while(ever < 100){
-		printf("Current ArrayPoint:%i, Current Point: %i, Verfügbare Kanten: %i, %i, %i, %i!\n", currentArrayPoint, currentPoint, allPoints[currentPoint].arrayEdges[0], allPoints[currentPoint].arrayEdges[1], allPoints[currentPoint].arrayEdges[2], allPoints[currentPoint].arrayEdges[3]);
-		switch(nextMove){
-		case 0: moveNorth(); break;
-		case 1: moveEast(); break;
-		case 2: moveSouth(); break;
-		case 3: moveWest(); break;
-		}
+		useNewPath(getNextPoint());;
 		ever++;
-
 	}
-
-
+	getNextPoint();
+	//printf("\n\n Zielpunkt:(%i,%i) Erster Rücksprung:%i",allPoints[getNextPoint()].coord[0], allPoints[getNextPoint()].coord[1], allPoints[getNextPoint()].edgeToPointBefore );
 
 /*
+
 	setNull();printStatusPoints();
 	moveSouth();printStatusPoints();
 	moveSouth();printStatusPoints();
@@ -297,8 +327,8 @@ int main(void) {
 	printf("Intersection: %d\n", Robot_GetIntersections());
 	//printf("Token: %d\n", Robot_Move(2, 0));
 
-	*/
 
+*/
 
 
 
